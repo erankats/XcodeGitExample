7fe8369938aeca358a1bcf3e71706ec23be0e617
Katsav Eran <erankatsav@gmail.com>
2018-06-22 12:51:30 +0300

    Initial Commit

---
+++ XcodeGitExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XcodeGitExample
+//
+//  Created by Eran Katsav on 22/06/2018.
+//  Copyright © 2018 Eran Katsav. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XcodeGitExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XcodeGitExample
+//
+//  Created by Eran Katsav on 22/06/2018.
+//  Copyright © 2018 Eran Katsav. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
